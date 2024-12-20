a04dd66278d11dc1f9e3f2d18b187b6038db1a30
sophia0805 <sophiawang8509@gmail.com>
2024-12-20 17:02:27 -0500

    idkkk

--- src/firebase.js
+++ src/firebase.js
@@ -22,4 +22,12 @@ firebase.firestore().settings({ experimentalForceLongPolling: true });
 
 const storage = firebase.storage();
 
+useEffect(() => {
+  const unloadCallback = () => {firebase.app().delete()}
+  window.addEventListener("beforeunload", unloadCallback);
+  return async () => {
+    window.removeEventListener("beforeunload", unloadCallback);
+  }
+}, [])
+
 export {auth, db, storage};
